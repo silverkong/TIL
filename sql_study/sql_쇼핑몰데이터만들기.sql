e6439bc22ab1c4e532b7fde9bbc5479403b01213
silverkong <bina25@nate.com>
2022-07-04 17:23:20 +0900

    study sql shopb db

--- sql_study/sql_쇼핑몰데이터만들기.sql
+++ sql_study/sql_쇼핑몰데이터만들기.sql
@@ -1,7 +1,18 @@
--- sqlex라는 schema 생성 (데이터베이스 생성)
-CREATE SCHEMA sqlex DEFAULT CHARACTER SET utf8;
+-- shopdb라는 schema 생성 (데이터베이스 생성)
+CREATE SCHEMA shopdb DEFAULT CHARACTER SET utf8mb4;
 
--- sqlex 사용
-USE sqlex;
+-- shopdb 사용
+USE shopdb;
 
--- 테이블 생성하기
+-- 회원 테이블 생성하기 (member)
+CREATE TABLE member(
+	memId VARCHAR(20) NOT NULL PRIMARY KEY,
+    memPwd VARCHAR(40) NOT NULL,
+    memName VARCHAR(20) NOT NULL,
+    memEmail VARCHAR(40) NOT NULL,
+    memZipCode CHAR(5),
+    memAddress VARCHAR(50),
+    memPhone VARCHAR(13) NOT NULL,
+    memDelete CHAR(1) DEFAULT 'N' NOT NULL,
+    memSince DATE NOT NULL
+);
