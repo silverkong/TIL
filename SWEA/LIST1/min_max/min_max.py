c808b43bddac5fda1c86bf7f60152e524dd8a443
silverkong <bina25@nate.com>
2022-10-26 15:59:54 +0900

    SWEA LIST 1 min_max

---
+++ SWEA/LIST1/min_max/min_max.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    N = int(input())
+    R = list(map(int, input().split()))
+    min = R[0]
+    max = 0
+    
+    for i in range(1, N):
+        if R[i] < min:
+            min = R[i]
+        elif R[i] > max:
+            max = R[i]
+    print("#", test_case, " ", max-min, sep='')
